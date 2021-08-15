b6aeca44d313916820614e11de666d0c82c6f103
haniputraa0607 <hanifmurtadha0607@gmail.com>
2021-08-15 17:35:31 +0700

    membuat dan menjalankan migration categories dan transactions

---
+++ database/migrations/2021_08_15_102729_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id_transaksi');
+            $table->integer('id_kategori');
+            $table->string('nominal_trans');
+            $table->string('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
